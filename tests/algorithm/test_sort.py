c546caa3b896cfe051f09f0ef2a63844e72120a4
Ben RdO <b.rouille.d.orfeuil@gmail.com>
2023-07-10 15:14:46 -0700

    test: use pytest fixture

--- tests/algorithm/test_sort.py
+++ tests/algorithm/test_sort.py
@@ -1,19 +1,29 @@
+import pytest
+
 from interview.algorithm.sort import bubblesort, quicksort
 
 
-def test_quicksort():
-    input = ([23, 12.5, 3, 32, -3, -100, 0, 4.25, 5, 1, 83], ["c", "z", "a", "n"])
-    expected = ([-100, -3, 0, 1, 3, 4.25, 5, 12.5, 23, 32, 83], ["a", "c", "n", "z"])
-    for i, e in zip(input, expected):
-        assert quicksort(i, inplace=False) == e
+@pytest.fixture
+def input():
+    return ([23, 12.5, 3, 32, -3, -100, 0, 4.25, 5, 1, 83], ["c", "z", "a", "n"])
 
 
-def test_quicksort_inplace():
-    input = [23, 12.5, 3, 32, -3, -100, 0, 4.25, 5, 1, 83]
-    quicksort(input, low=3, high=7)
-    assert input == [23, 12.5, 3, -100, -3, 0, 4.25, 32, 5, 1, 83]
+@pytest.fixture
+def output():
+    return ([-100, -3, 0, 1, 3, 4.25, 5, 12.5, 23, 32, 83], ["a", "c", "n", "z"])
 
 
-def test_bubblesort():
-    input = [12, 3, 1, -1, 30, 14, 12, 0, 25, 9, 4]
-    assert bubblesort(input) == [-1, 0, 1, 3, 4, 9, 12, 12, 14, 25, 30]
+def test_quicksort(input, output):
+    for i, o in zip(input, output):
+        assert quicksort(i, inplace=False) == o
+
+
+def test_quicksort_inplace(input, output):
+    for i, o in zip(input, output):
+        quicksort(i)
+        assert i == o
+
+
+def test_bubblesort(input, output):
+    for i, o in zip(input, output):
+        assert bubblesort(i) == o
